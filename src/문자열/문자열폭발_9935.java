d6c0165fc0c88347929460be230eb27d10d00030
OOOIOOOIO <polite159@gmail.com>
2022-10-13 18:06:09 +0900

    stack?

---
+++ src/문자열/문자열폭발_9935.java
@@ -0,0 +1,25 @@
+package 문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class 문자열폭발_9935 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
+		
+		String str = br.readLine();
+		String boomb = br.readLine();
+		
+		for(int i = 0; i <= str.length() - boomb.length(); i++) {
+			
+			while(true) {
+				
+			}
+		}
+	}
+}
