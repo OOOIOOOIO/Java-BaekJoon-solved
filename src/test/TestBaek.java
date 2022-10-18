72b5445a30c7b0d33a5867671e0ab3dbb69815c3
OOOIOOOIO <polite159@gmail.com>
2022-10-18 23:56:20 +0900

    c

--- src/test/TestBaek.java
+++ src/test/TestBaek.java
@@ -6,68 +6,10 @@ import java.io.InputStreamReader;
 import java.util.*;
  
 public class TestBaek {
+	
+    public static void main(String[] args) throws IOException {
+    	String str = "123456";
+    	
 
-
-	    static int[][] d;
-	    static int[][] d2;
-	    static int[][] arr;
-
-	    static int solve(int i, int j) {
-	        if (d[i][j] != -1)
-	            return d[i][j];
-	        
-	        if (i == n - 1)
-	            return arr[i][j];
-	        
-	        d[i][j] = 0;
-	        
-	        if (j == 0)
-	            d[i][j] = arr[i][j] + Math.max(solve(i + 1, 0), solve(i + 1, 1));
-	        else if (j == 1)
-	            d[i][j] = arr[i][j] + Math.max(solve(i + 1, 0), Math.max(solve(i + 1, 1), solve(i + 1, 2)));
-	        else if (j == 2)
-	            d[i][j] = arr[i][j] + Math.max(solve(i + 1, 1), solve(i + 1, 2));
-	        return d[i][j];
-	    }
-
-	    static int solve2(int i, int j) {
-	        if (d2[i][j] != 987654321)
-	            return d2[i][j];
-	        
-	        if (i == n - 1)
-	            return arr[i][j];
-	        
-	        d2[i][j] = 0;
-	        
-	        if (j == 0)
-	            d2[i][j] = arr[i][j] + Math.min(solve2(i + 1, 0), solve2(i + 1, 1));
-	        else if (j == 1)
-	            d2[i][j] = arr[i][j] + Math.min(solve2(i + 1, 0), Math.min(solve2(i + 1, 1), solve2(i + 1, 2)));
-	        else if (j == 2)
-	            d2[i][j] = arr[i][j] + Math.min(solve2(i + 1, 1), solve2(i + 1, 2));
-	        return d2[i][j];
-	    }
-
-	    static int n;
-	    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-	    public static void main(String[] args) throws IOException {
-	        n = Integer.parseInt(br.readLine());
-	        d = new int[n][3];
-	        d2 = new int[n][3];
-	        arr = new int[n][3];
-	        for (int i = 0; i < n; i++) {
-	            StringTokenizer st = new StringTokenizer(br.readLine());
-	            arr[i][0] = Integer.parseInt(st.nextToken());
-	            arr[i][1] = Integer.parseInt(st.nextToken());
-	            arr[i][2] = Integer.parseInt(st.nextToken());
-	            Arrays.fill(d[i], -1);
-	            Arrays.fill(d2[i], 987654321);
-	        }
-	        int max = Math.max(Math.max(solve(0, 0), solve(0, 1)), solve(0, 2));
-	        int min = Math.min(Math.min(solve2(0, 0), solve2(0, 1)), solve2(0, 2));
-	        System.out.println(max + " " + min);
-
-
-	    }
-	}
+    }
+}
--- src/문자열/문자열폭발_9935.java
+++ src/문자열/문자열폭발_9935.java
@@ -6,6 +6,11 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
+/*
+ * 문자열 검사해서 삭제 혹은 뭐 이상한 거 할 떄
+ * 기준은 삭제할 문자열이다.
+ * 
+ */
 public class 문자열폭발_9935 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,12 +19,32 @@ public class 문자열폭발_9935 {
 		
 		String str = br.readLine();
 		String boomb = br.readLine();
+		int boLen = boomb.length();
+		Stack<Character> stack = new Stack<Character>();
 		
-		for(int i = 0; i <= str.length() - boomb.length(); i++) {
+		int idx = 0;
+		
+		for(int i = 0; i < str.length(); i++) {
+			// 일단 넣어
+			stack.push(str.charAt(i));
 			
-			while(true) {
+			// 검색 시작 
+			if(stack.size() >= boLen) {
+				
+				// 검색
+				for(int j = 0; j < boLen; j++) {
+					
+					if(stack.get(stack.size() - boLen + j) != boomb.charAt(j)) {
+						break;
+					}
+				}
+				
+				
 				
 			}
+			
+			
 		}
+
 	}
 }
