4d0258be92df7b1efb267dd9696a1393c8c2d459
OOOIOOOIO <polite159@gmail.com>
2022-10-19 00:04:12 +0900

    stack.get(stack.size() - boLen + j), 문자열 검사해서 삭제 혹은 뭐 이상한 거 할 떄

--- src/문자열/문자열폭발_9935.java
+++ src/문자열/문자열폭발_9935.java
@@ -8,21 +8,23 @@ import java.util.StringTokenizer;
 
 /*
  * 문자열 검사해서 삭제 혹은 뭐 이상한 거 할 떄
- * 기준은 삭제할 문자열이다.
+ * 이렇게 하면 되는구나
+ * 
+ * 
+mirkovC4nizCC44
+C4
  * 
  */
 public class 문자열폭발_9935 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = null;
 		
 		String str = br.readLine();
 		String boomb = br.readLine();
 		int boLen = boomb.length();
 		Stack<Character> stack = new Stack<Character>();
 		
-		int idx = 0;
 		
 		for(int i = 0; i < str.length(); i++) {
 			// 일단 넣어
@@ -30,21 +32,38 @@ public class 문자열폭발_9935 {
 			
 			// 검색 시작 
 			if(stack.size() >= boLen) {
+				// 일치 유무
+				boolean flag = true;
 				
 				// 검색
 				for(int j = 0; j < boLen; j++) {
 					
+					// stack.get(스택길이 - 폭탄길이 + 인덱스)를 해줌으로써 
 					if(stack.get(stack.size() - boLen + j) != boomb.charAt(j)) {
+						flag = false;
 						break;
 					}
 				}
 				
-				
-				
+				// 일치했다면
+				if(flag) {
+					for(int j = 0; j < boLen; j++) {
+						stack.pop();
+						
+					}
+				}
 			}
-			
+		}
+		
+		if(stack.isEmpty()) {
+			System.out.println("FRULA");
+		}
+		else {
+			for(Character c : stack) {
+				sb.append(c);
+			}
+			System.out.println(sb.toString());
 			
 		}
-
 	}
 }
