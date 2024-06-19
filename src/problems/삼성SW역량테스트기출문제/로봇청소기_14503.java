f7140ce66b20f6964033411ec12adc23448f24ff
OOOIOOOIO <polite159@gmail.com>
2024-06-19 11:51:31 +0900

    흠 dfs에서 return을 저런식으로도 쓰는구만

---
+++ src/problems/삼성SW역량테스트기출문제/로봇청소기_14503.java
@@ -0,0 +1,100 @@
+package problems.삼성SW역량테스트기출문제;
+
+import java.util.*;
+import java.io.*;
+
+
+public class 로봇청소기_14503{
+
+    static int N, M;
+
+    static int[][] map;
+    static int cnt = 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+
+        st = new StringTokenizer(br.readLine());
+        int r = Integer.parseInt(st.nextToken()); //y
+        int c = Integer.parseInt(st.nextToken()); //x
+        int dir = Integer.parseInt(st.nextToken());
+
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < M; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+
+            }
+        }
+
+        dfs(r, c, dir);
+
+        System.out.println(cnt);
+    }
+
+    static int[] dy = new int[]{-1, 0, 1, 0}; // 북(0), 동(1), 남(2), 서(3)
+    static int[] dx = new int[]{0, -1, 0, 1};
+
+    public static void dfs(int y, int x, int dir){
+        map[y][x] = -1;
+
+        //청소
+        for(int i = 0; i < 4; i++){
+
+            dir = (dir + 3) % 4;
+
+            int ny = y + dy[dir];
+            int nx = x + dy[dir];
+
+            if(nx >= 0 && ny >= 0 && nx < N && ny < M) {
+                if(map[ny][nx] == 0) {
+                    cnt++;
+                    dfs(nx, ny, dir);
+                    return; // 노드하나에 대해 청소 끝나면 끝내기
+                }
+            }
+        }
+
+        //후진
+        int d = (dir + 2) % 4;
+        int bx = x + dx[d];
+        int by = y + dy[d];
+        if(bx >= 0 && by >= 0 && bx < N && by < M && map[bx][by] != 1) {
+            dfs(bx, by, dir); //후진이니까 바라보는 방향은 유지
+        }
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
