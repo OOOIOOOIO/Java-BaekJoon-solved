baff0ab3e983aeb5159ba04a6d0c88333f216a40
OOOIOOOIO <polite159@gmail.com>
2024-06-28 16:00:31 +0900

    re

--- src/problems/category/dfs_bfs_그래프순회/DSLR_9019_XXX.java
+++ src/problems/category/dfs_bfs_그래프순회/DSLR_9019_XXX.java
@@ -8,7 +8,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class DSLR_9019 {
+public class DSLR_9019_XXX {
 	static boolean[] visited;
 	static String[] ans;
 	
