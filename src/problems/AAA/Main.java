8fb04a012a0233c68434321de42321327fa76778
OOOIOOOIO <polite159@gmail.com>
2024-07-22 15:00:52 +0900

    흠 조합을 만들고 비트 ㅎ믛믐

--- src/problems/AAA/Main.java
+++ src/problems/AAA/Main.java
@@ -6,9 +6,47 @@ import java.io.*;
 public class Main{
 
 
-    public static void main(String[] args) {
-        double a = (1.0 / 185.0);
+    static int N, M;
 
-        System.out.println(String.format("%.5f", a));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        String[][] map = new String[N][M];
+        List<HashSet<String>> list = new ArrayList<>();
+        for(int i = 0; i < M; i++){
+            list.add(new HashSet<String>());
+        }
+
+        int max = -1;
+        for(int i = 0; i < N; i++){
+            String[] str = br.readLine().split("");
+
+            map[i] = str;
+        }
+
+        for(int j = 0; j < M; j++){
+            HashSet<String> set = list.get(j);
+
+            for(int i = 0; i < N; i++){
+                if(!(set.contains(map[i][j])) && !map[i][j].equals(".")){
+                    set.add(map[i][j]);
+                }
+
+            }
+
+            max = Math.max(max, set.size());
+        }
+
+        if(max == -1){
+            System.out.println(1);
+        }
+        else{
+            System.out.println(max);
+        }
+
     }
 }
