1fb618fcc2773c66b0b86bccac3ee21590e96629
OOOIOOOIO <polite159@gmail.com>
2024-10-24 00:02:18 +0900

    bottom-up, Integer[]

--- src/problems/AAA/Main.java
+++ src/problems/AAA/Main.java
@@ -6,16 +6,68 @@ import java.io.*;
 import java.util.*;
 import java.io.*;
 
+import java.util.*;
+import java.io.*;
+
 public class Main{
 
-    public static void main(String[] args) {
-        String a = "it";
-        String b = "abd";
-        String c = "im";
+    static final int INF = Integer.MAX_VALUE;
+    static int N;
+    static int[] dp; //[숫자] = 횟수
 
-        System.out.println(a.compareTo(c));
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        N = Integer.parseInt(br.readLine());
+
+        dp = new int[N+1];
+        Arrays.fill(dp, INF);
+        dp[N] = 0; // 시작값
+        solve(N, 0);
+
+        System.out.println(dp[1]);
+
+
 
     }
+
+    public static void solve(int num, int cnt){
+
+        // 0이거나 0보다 작을 때 종료
+        if(num <= 0){
+            return;
+        }
+
+        // 1일 때 종료
+        if(num == 1){
+            dp[num] = Math.min(dp[num], cnt);
+            return;
+        }
+
+        if(num % 3 == 0){
+            dp[num] = Math.min(dp[num], cnt);
+            solve(num/3, cnt+1);
+        }
+
+        if(num % 2 == 0){
+            dp[num] = Math.min(dp[num], cnt);
+            solve(num/2, cnt+1);
+        }
+
+        dp[num] = Math.min(dp[num], cnt);
+        solve(num-1, cnt+1);
+
+
+
+
+
+
+    }
+
+
 }
+
 
 
--- src/problems/category/동적계획법/_1로만들기_1463_OOO.java
+++ src/problems/category/동적계획법/_1로만들기_1463_OOO.java
@@ -5,42 +5,56 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class _1로만들기_1463_OOO {
-	 
-		static Integer[] dp;
-	 
-		public static void main(String[] args) throws IOException {
-	 
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	 
-			int N = Integer.parseInt(br.readLine());
-	 
-			dp = new Integer[N + 1];
-			dp[0] = dp[1] = 0;
-			System.out.print(recur(N));
-	 
-		}
-	     // " + 1은 cnt임"
-		static int recur(int N) {
-			if (dp[N] == null) {
-				// 6으로 나눠지는 경우 
-				if (N % 6 == 0) {
-					dp[N] = Math.min(recur(N - 1), Math.min(recur(N / 3), recur(N / 2))) + 1;
-				}
-				// 3으로만 나눠지는 경우 
-				else if (N % 3 == 0) {
-					dp[N] = Math.min(recur(N / 3), recur(N - 1)) + 1;
-				}
-				// 2로만 나눠지는 경우 
-				else if (N % 2 == 0) {
-					dp[N] = Math.min(recur(N / 2), recur(N - 1)) + 1;
-				}
-				// 2와 3으로 나누어지지 않는 경우
-				else {
-					dp[N] = recur(N - 1) + 1;
-				}
-			}
-			
-			return dp[N];
-		}
-	 
+
+	static int N;
+	static Integer[] dp; //[숫자] = 횟수
+
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+		N = Integer.parseInt(br.readLine());
+
+
+		dp = new Integer[N+1]; // 처음 들어온 게 가장 적은 횟수이므로 null인 경우에만 확인할 수 있게
+
+		// bottom-up임, 0,1은 0으로 초기화
+		dp[0] = 0;
+		dp[1] = 0;
+		solve(N);
+
+		System.out.println(dp[N]);
+
+
+
 	}
+
+	public static int solve(int num){
+
+		if(dp[num] == null){
+
+			if(num % 3 == 0 && num % 2 == 0){
+				dp[num] = Math.min(solve(num-1) + 1, Math.min(solve(num/3) + 1, solve(num/2) + 1));
+			}
+			else if(num % 3 == 0){
+				dp[num] = Math.min(solve(num/3) + 1, solve(num-1) + 1);
+			}
+			else if(num % 2 == 0){
+				dp[num] = Math.min(solve(num/2) + 1, solve(num-1) + 1);
+			}
+			else{
+				dp[num] = solve(num-1) + 1;
+			}
+
+		}
+
+
+
+		return dp[num];
+
+
+
+	}
+	 
+}
